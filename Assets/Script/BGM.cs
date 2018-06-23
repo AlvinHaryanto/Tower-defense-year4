b3707138ff2ddb7c2210a23bb9b73f1f6eb126fc
AlvinHaryanto <36879496+AlvinHaryanto@users.noreply.github.com>
2018-06-23 17:37:21 +0700

    MUSIC + ANIMASI BENTO

---
+++ Assets/Script/BGM.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM : MonoBehaviour {
+
+    public static BGM instance;
+    public AudioSource source;
+
+    void Awake() { //SINGLETOD
+
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlayBGM(AudioClip clip)
+    {
+        source.clip = clip;
+        source.Play();
+
+    }
+
+
+}
---
+++ Assets/Script/TempelBGM.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempelBGM : MonoBehaviour {
+
+    public AudioClip clip;
+    private void Start()
+    {
+        BGM.instance.source.volume = 0.5f;
+        BGM.instance.PlayBGM(clip);
+    }
+    
+
+}
